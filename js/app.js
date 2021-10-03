e011f08e750151ebc80a0cc568d6388ac9b9f98b
Nevena Milivojevic <neca876@gmail>
2021-10-03 21:08:29 +0200

    added the add to todo list functionality  and items left count

--- js/app.js
+++ js/app.js
@@ -1,37 +1,90 @@
 (function () {
-
   const TODOController = {
+    todoList: [],
+    addTodo: function (tekst, status) {
+      this.todoList.push({
+        tekst: tekst,
+        status: status,
+      });
 
-      addItemToList: function () {
-        
-      }
-  }
+      UIController.displayTodo();
+    },
 
-// UI CONTROLLER
+    leftTodos: function () {
+      const leftTodos = this.todoList.filter((todo) => {
+        return !todo.status;
+      });
+      
+      if(leftTodos.length < 0) return;
+      
+      const count = document.querySelector(UIController.domEle.count);
+      count.textContent = `${leftTodos.length} items left`;
+    },
+  };
+
   const UIController = {
-
     domEle: {
       input: "#todo",
       list: ".todo-list",
+      form: "#form",
+      count: ".count",
     },
 
+    getInput: function () {
+      return document.querySelector(this.domEle.input).value;
+    },
 
+    addTodoToList: function (data) {
+      // prikazivanje u listi tj renderovanje
+      let markUp;
+      markUp = `
+        <li class="todo" draggable="true">
+          <label>
+            <input type="checkbox" name="light" ${data.status ? "checked" : ""}>
+            <span class="custom-checkbox">
+              <img src="./images/icon-check.svg" alt="checkmark">
+            </span>
+          </label>
+          <span class="todo-text">${data.tekst}</span>
+        </li>
+        `;
+
+      document
+        .querySelector(this.domEle.list)
+        .insertAdjacentHTML("beforeend", markUp);
+      TODOController.leftTodos();
+    },
+
+    displayTodo: function () {
+      const todos = TODOController.todoList;
+      document.querySelector(this.domEle.list).innerHTML = "";
+      todos.forEach((todo, index) => {
+        this.addTodoToList(todo);
+      });
+    },
   };
 
   const MainController = {
     setAllEvents: function () {
+      // dogadjaj unos u input polje
       document
         .querySelector(UIController.domEle.input)
         .addEventListener("keypress", function (e) {
-          console.log(e);
+          if (e.keyCode == 13) {
+            TODOController.addTodo(UIController.getInput(), false);
+          }
         });
-    }
-  }
 
-  window.addEventListener('load', function () {
+      // submitovanje forme
+      document
+        .querySelector(UIController.domEle.form)
+        .addEventListener("submit", function (e) {
+          e.preventDefault();
+        });
+    },
+  };
+
+  window.addEventListener("load", function () {
     MainController.setAllEvents();
-  })
-
-
-
+  });
 })();
